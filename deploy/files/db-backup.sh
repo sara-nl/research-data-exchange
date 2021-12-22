f10e72f175a240a594dd981520bc03bd01018d8f
Mike Kotsur <miccots@gmail.com>
2021-12-22 14:31:35 +0100

    DB backup changed. See #15

--- deploy/files/db-backup.sh
+++ deploy/files/db-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
-docker exec -it rdx_postgres_1 pg_dump -U postgres | gzip > "$BASEDIR"/db-backup-"`date +%d-%m-%Y_%H:%M:%S`".sql.gz
+docker exec -t rdx_postgres_1 pg_dump -U postgres | gzip > "$BASEDIR"/db-backup-"`date +%d-%m-%Y_%H:%M:%S`".sql.gz
