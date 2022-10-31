1cb78df7668257457e2c87ebe497591051f29031
Tim Kok <tim.kok@surf.nl>
2022-10-31 15:35:45 +0100

    Setup token/user validation

--- backend/v2/librarian/librarian.py
+++ backend/v2/librarian/librarian.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session
 
+from common.api.dependencies import get_rdx_user
 from common.db.db_client import DBClient
 from common.models.rdx_models import (
     RdxDataset,
     RdxDatasetReadWithShare,
     RdxDatasetUpdate,
+    RdxUser,
 )
 
 app = FastAPI()
 
 db = DBClient()
 
+# TODO: use .env to set origins and CORS
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -29,33 +32,53 @@ app.add_middleware(
 )
 
 
-# TODO add token middleware
 @app.get("/api/dataset/{dataset_id}", response_model=RdxDatasetReadWithShare)
-def get_dataset(
-    *, session: Session = Depends(db.get_session_dependency), dataset_id: int
-):
-    rdx_dataset = session.get(RdxDataset, dataset_id)
-    if not rdx_dataset:
-        raise HTTPException(status_code=404, detail="Dataset not found")
-    return rdx_dataset
-
-
-# TODO add token middleware
-@app.patch("/api/dataset/{dataset_id}", response_model=RdxDatasetReadWithShare)
 def get_dataset(
     *,
     session: Session = Depends(db.get_session_dependency),
     dataset_id: int,
-    dataset: RdxDatasetUpdate
+    rdx_user: RdxUser = Depends(get_rdx_user),
+):
+    rdx_dataset = session.get(RdxDataset, dataset_id)
+    if not rdx_dataset:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Dataset not found"
+        )
+
+    if rdx_user.id not in [rdx_dataset.researcher_id, rdx_dataset.data_steward_id]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden resource"
+        )
+
+    return rdx_dataset
+
+
+@app.patch("/api/dataset/{dataset_id}", response_model=RdxDatasetReadWithShare)
+def publish_dataset(
+    *,
+    session: Session = Depends(db.get_session_dependency),
+    rdx_user: RdxUser = Depends(get_rdx_user),
+    dataset_id: int,
+    dataset: RdxDatasetUpdate,
 ):
     rdx_dataset = session.get(RdxDataset, dataset_id)
     if not rdx_dataset:
         raise HTTPException(status=404, detail="Dataset not found")
+
+    # TODO: decide whether this action should be limited to researcher/data steward
+    if rdx_user.id not in [rdx_dataset.researcher_id, rdx_dataset.data_steward_id]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden resource"
+        )
+
     if not rdx_dataset.published and dataset.published:
         rdx_dataset.published_at = datetime.now()
+
     for key, value in dataset.dict(exclude_unset=True).items():
         setattr(rdx_dataset, key, value)
+
     session.add(rdx_dataset)
     session.commit()
     session.refresh(rdx_dataset)
+
     return rdx_dataset
