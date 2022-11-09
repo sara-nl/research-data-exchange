9a4d585fced831d54bbfe59250b9398b92375d06
Tim Kok <tim.kok@surf.nl>
2022-11-09 17:03:57 +0100

    Send confirmation of dataset acceptance to share owner

--- backend/v2/sharer/pipeline/users.py
+++ backend/v2/sharer/pipeline/users.py
@@ -5,7 +5,9 @@ from common.email.mail_client import MailClient
 from common.models.rdx_models import RdxShare, RdxUser, ShareStatus
 from common.owncloud.owncloud_client import ShareInfo
 
-from .email import get_message, get_misconfiguration_message
+from .email import (get_misconfiguration_message,
+                    get_publication_message_researcher,
+                    get_publication_message_share_owner)
 
 
 def create_users(db: DBClient, new_shares: list[ShareInfo]) -> list[ShareInfo]:
@@ -70,44 +72,56 @@ def notify(db: DBClient, shared_dirs: list[ShareInfo]):
 
 
 def send_email(shared_dir: ShareInfo) -> bool:
-    mail_client = None
+    mail_clients = []
 
     if shared_dir.rdx_share.share_status == ShareStatus.dataset_accepted:
-        mail_client = get_dataset_ready_email(shared_dir)
+        mail_clients.append(get_dataset_ready_email_researcher(shared_dir))
+        mail_clients.append(get_dataset_ready_email_share_owner(shared_dir))
     if shared_dir.rdx_share.share_status == ShareStatus.invalid_permissions:
         problem = "the RDX user does not have permission to reshare the dataset"
         solution = "check the 'can share' box for the RDX user in Research Drive"
-        mail_client = get_misconfiguration_email(shared_dir, problem, solution)
+        mail_clients.append(get_misconfiguration_email(shared_dir, problem, solution))
     if shared_dir.rdx_share.share_status == ShareStatus.missing_conditions:
         problem = "the shared directory is missing a conditions.pdf file"
         solution = "add a conditions.pdf file to the directory shared with RDX"
-        mail_client = get_misconfiguration_email(shared_dir, problem, solution)
+        mail_clients.append(get_misconfiguration_email(shared_dir, problem, solution))
     if shared_dir.rdx_share.share_status == ShareStatus.missing_dataset_config:
         problem = "the shared directory is missing a dataset.yml file with metadata and configuration options for the dataset"
         solution = "add a dataset.yml file to the directory shared with RDX"
-        mail_client = get_misconfiguration_email(
-            shared_dir, problem, solution, show_dataset_config_example=True
+        mail_clients.append(
+            get_misconfiguration_email(
+                shared_dir, problem, solution, show_dataset_config_example=True
+            )
         )
     if shared_dir.rdx_share.share_status == ShareStatus.invalid_dataset_config:
         problem = "the configuration specified in the dataset.yml file is incomplete or invalid"
         solution = "change the contents of the dataset.yml file, see the example below for a valid example"
-        mail_client = get_misconfiguration_email(
-            shared_dir, problem, solution, show_dataset_config_example=True
+        mail_clients.append(
+            get_misconfiguration_email(
+                shared_dir, problem, solution, show_dataset_config_example=True
+            )
         )
 
-    # Not every case will require a mail to be sent
-    if mail_client:
+    for mail_client in mail_clients:
         mail_client.mail()
 
-    return bool(mail_client)
+    return bool(mail_clients)
 
 
-def get_dataset_ready_email(shared_dir: ShareInfo) -> MailClient:
+def get_dataset_ready_email_researcher(shared_dir: ShareInfo) -> MailClient:
     researcher: RdxUser = shared_dir.researcher
     return MailClient(
         receiver=researcher.email,
         subject=f"Dataset {shared_dir.rdx_share.path} available for publication",
-        message=get_message(researcher, shared_dir.rdx_share),
+        message=get_publication_message_researcher(researcher, shared_dir.rdx_share),
+    )
+
+
+def get_dataset_ready_email_share_owner(shared_dir: ShareInfo) -> MailClient:
+    return MailClient(
+        receiver=shared_dir.rdx_share.additional_info_owner,
+        subject=f"Dataset {shared_dir.rdx_share.path} successfully shared with RDX",
+        message=get_publication_message_share_owner(shared_dir.rdx_share),
     )
 
 
