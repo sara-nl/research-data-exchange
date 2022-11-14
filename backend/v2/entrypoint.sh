820f2926afb441ee4ff07817bd96534462071c8b
Tim Kok <tim.kok@surf.nl>
2022-11-14 18:37:08 +0100

    Cleanup code

--- backend/v2/entrypoint.sh
+++ backend/v2/entrypoint.sh
@@ -17,15 +17,6 @@ elif [[ ${APPLICATION} == "librarian" ]]; then
     echo "Starting librarian..."
     pipenv run uvicorn --host 0.0.0.0 librarian.librarian:app
     exit 0
-elif [[ ${APPLICATION} == "linting" ]]; then
-    echo "Running linting..."
-    # pylint and flake8 not yet fixed
-    pipenv run pylint --recursive=yes common sharer librarian || true
-    pipenv run flake8 . || true
-    pipenv run isort --check-only .
-    pipenv run black --check .
-    pipenv run bandit .
-    exit 0
 else
     echo "Unknown APPLICATION environment variable... exiting"
     exit 1
